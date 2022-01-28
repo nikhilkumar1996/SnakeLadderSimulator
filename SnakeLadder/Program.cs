3b71b16c3c95c58617ba9dbcf52b101a01392e32
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-01-29 02:51:11 +0530

    [Nikhil Kumar]:UC-3 files Added

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -3,5 +3,4 @@
 Console.WriteLine("Welcome to Snake Ladder Program");
 
 SnakeAndLadderGame SnakeAndLadder =new SnakeAndLadderGame();
-int a= SnakeAndLadder.DiceRoll();
-Console.WriteLine(a);
+SnakeAndLadder.Play();
--- SnakeLadder/SnakeAndLadderGame.cs
+++ SnakeLadder/SnakeAndLadderGame.cs
@@ -8,12 +8,34 @@ namespace SnakeLadder
 {
     public class SnakeAndLadderGame
     {
+        const int NoPlay = 0, Ladder = 1, Snake = 2;
         int playerPosition = 0;
         Random random = new Random();
         public int DiceRoll()
         {
             return random.Next(1, 7);
         }
+        public void Play()
+        {
+            int roll = random.Next(0, 3);
+            switch (roll)
+            {
+                case NoPlay:
+                    Console.WriteLine("Player hasn't rolled the dice");
+                    break;
+
+                case Ladder:
+                    this.playerPosition += DiceRoll();
+                    Console.WriteLine(this.playerPosition);
+                    break;
+
+                case Snake:
+                    this.playerPosition = playerPosition - DiceRoll();
+                    Console.WriteLine(this.playerPosition);
+                    break;
+
+            }
+        } 
         
     }
 }
