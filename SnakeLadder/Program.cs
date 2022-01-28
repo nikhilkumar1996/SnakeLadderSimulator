a48e83326df04dfe165ce0481e92dbb1061e2ca0
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-01-29 02:27:44 +0530

    [Nikhil Kumar]:UC-2 program files added

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,3 +1,7 @@
 ﻿using SnakeLadder;
 
 Console.WriteLine("Welcome to Snake Ladder Program");
+
+SnakeAndLadderGame SnakeAndLadder =new SnakeAndLadderGame();
+int a= SnakeAndLadder.DiceRoll();
+Console.WriteLine(a);
