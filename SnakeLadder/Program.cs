d511e8d0b95c075ce71d29f570ca9eba685a7c76
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-01-31 22:00:18 +0530

    [Nikhil Kumar]:Two players added to play

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -1,6 +1,39 @@
-﻿using SnakeLadder;
+﻿using System;
+namespace SnakeLadder {
 
-Console.WriteLine("Welcome to Snake Ladder Program");
+    public class Program
+    {
 
-SnakeAndLadderGame SnakeAndLadder =new SnakeAndLadderGame();
-SnakeAndLadder.Play();
+        public static void Main()
+        {
+
+            Console.WriteLine("Welcome to Snake Ladder Program");
+
+            SnakeAndLadderGame SnakeAndLadder = new SnakeAndLadderGame();
+            
+
+            int [] players =new int[2] { 1, 2};
+            
+            
+                foreach (int items in players)
+                {
+                    
+                    int PLAYER = SnakeAndLadder.Play(items);
+                    
+
+
+
+                    if ( PLAYER==100)
+                    {
+                        Console.WriteLine( "Player {0} wins",items);
+                    }
+                    return;
+                    
+
+
+
+                }
+
+            }
+        }
+    }
--- SnakeLadder/SnakeAndLadderGame.cs
+++ SnakeLadder/SnakeAndLadderGame.cs
@@ -8,56 +8,80 @@ namespace SnakeLadder
 {
     public class SnakeAndLadderGame
     {
-        const int NoPlay = 0, Ladder = 1, Snake = 2, WinningPosition=100;
-        int playerPosition = 0, count; 
+        const int NoPlay = 0, Ladder = 1, Snake = 2, WinningPosition = 100;
+        int playerPosition = 0, count = 0;
         Random random = new Random();
         public int DiceRoll()
         {
             count++;
             return random.Next(1, 7);
-            
+
         }
-        public void Play()
+        public int Play(int player)
         {
-            while (this.playerPosition < WinningPosition)
+            while (this.playerPosition <= WinningPosition)
             {
 
                 int roll = random.Next(0, 3);
                 switch (roll)
                 {
                     case NoPlay:
-                        Console.WriteLine("Player hasn't rolled the dice");
-                        break;
+                        {
+                            Console.WriteLine("Player hasn't rolled the dice");
+
+                            return 0;
+                        }
+                        
+                        
+                       
+                        
 
                     case Ladder:
-                        if (this.playerPosition + DiceRoll() <= 100)
+                        if (this.playerPosition + DiceRoll() < 100)
                         {
                             this.playerPosition += DiceRoll();
 
+                            this.playerPosition += DiceRoll();
+                            return this.playerPosition;
+
                         }
+                        if (this.playerPosition + DiceRoll() == 100)
+                        {
+                            Console.WriteLine("Winner",this.playerPosition);
+                            return 100;
+                        }
+                         
+                        
                         break;
 
                     case Snake:
-                        if (this.playerPosition - DiceRoll() >= 0)
+                        if (this.playerPosition - DiceRoll() > 0)
                         {
                             this.playerPosition = playerPosition - DiceRoll();
+                            return this.playerPosition;
 
                         }
 
                         else
                         {
                             this.playerPosition = 0;
+                            return this.playerPosition;
                         }
 
-                        break;
+                        
+                  
+                    
 
 
                 }
-                Console.WriteLine("PlayerPosition="+this.playerPosition);
-            
+                Console.WriteLine("PlayerPosition=" + this.playerPosition);
+
             }
-            Console.WriteLine("Count of Dice Rolled="+count);
-        } 
-        
+            Console.WriteLine("Count of Dice Rolled=" + count);
+            
+            return this.playerPosition;
+            
+        }
+
     }
 }
